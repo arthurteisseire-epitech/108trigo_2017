98328217ab7c8acb7bdf0260e1d90382df34e185
arthur <arthur.teisseire@epitech.eu>
2018-03-16 10:51:47 +0100

    parsing seems to work as expected

--- parse.py
+++ parse.py
@@ -1,16 +1,17 @@
+from math import sqrt
 import argparse
 import sys
 
 def parse_all():
     fun_arr = ["EXP", "COS", "SIN", "COSH", "SINH"]
-    if (sys.argv[1] not in fun_arr)
+    if (sys.argv[1] not in fun_arr):
         exit(84)
     nb_ai = len(sys.argv) - 2
     square = sqrt(nb_ai)
-    if (square != int(square))
+    if (square != int(square)):
         exit(84)
     parser = argparse.ArgumentParser()
-    parser.add_argument("fun", type=float)
+    parser.add_argument("fun", type=str)
     parser.add_argument("ai", type=float, nargs = len(sys.argv) - 2)
     args = parser.parse_args()
     return (args)
